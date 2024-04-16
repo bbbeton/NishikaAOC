f5b4a27a4559c68556552b33bc22a9bc2ddb50b4
kosmilion <krzysztof.kosmalski24@gmail.com>
2024-04-16 11:52:54 +0200

    day2_2

--- KK/AOC_2015/day2/day2.py
+++ KK/AOC_2015/day2/day2.py
@@ -3,6 +3,7 @@ l = 0
 w = 0
 h = 0
 sum = 0
+ribbon = 0
 with open(filename) as file:
     for line in file:
         line = line.strip()
@@ -10,5 +11,7 @@ with open(filename) as file:
         l = int(line[0])
         w = int(line[1])
         h = int(line[2])
-        sum += 2 * l * w + 2 * w * h + 2 * h * l + min(l * w, w * h, h * l)
+        sum += 2 * (l * w + w * h + h * l) + min(l * w, w * h, h * l)
+        ribbon += 2 * min(l + w, w + h, h + l) + l * w * h
 print(sum)
+print(ribbon)
